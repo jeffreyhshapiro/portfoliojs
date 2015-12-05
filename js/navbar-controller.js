4cd07fb9be3af76616235b6999b52d39746bf066
unknown <jeffreyhshapiro@gmail.com>
2015-12-04 23:46:51 -0500

    added dev activity section to navbar, updated jquery for page navigation all hail google

--- js/navbar-controller.js
+++ js/navbar-controller.js
@@ -1,9 +1,35 @@
 $(document).ready(function(){
 
   $("#about-me").hide();
+  $("#skills").hide();
+  $("#dev-activity").hide();
+  $("#contact-me").hide();
   $("#nav-about-me").on("click",function(){
     $("#introduction").hide();
+    $("#skills").hide();
+    $("#dev-activity").hide();
     $("#about-me").show();
+  });
+
+  $("#nav-skills").on("click", function(){
+    $("#introduction").hide();
+    $("#about-me").hide();
+    $("#dev-activity").hide();
+    $("#skills").show();
   })
+
+  $("#nav-home").on("click", function(){
+    $("#about-me").hide();
+    $("#skills").hide();
+    $("#dev-activity").hide();
+    $("#introduction").show();
+  });
+
+  $("#nav-dev-activity").on("click"), function(){
+    $("#about-me").hide();
+    $("#skills").hide();
+    $("#introduction").hide();
+    $("#dev-activity").show();
+  }
 
 });
