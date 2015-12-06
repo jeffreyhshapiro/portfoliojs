dafc8c151953b878bae53bef8564ce8ced0f1f21
unknown <jeffreyhshapiro@gmail.com>
2015-12-06 11:18:11 -0500

    began incorporating github API into portfolio. table displays object object

--- js/github-api-homework.js
+++ js/github-api-homework.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
-     
+  $.ajax({
+    type: "GET",
+    url: "https://api.github.com/users/jeffreyhshapiro/repos",
+    success: function(repos){
+      console.log(repos);
+      for (i = 0; i < repos.length; i++);
+      $(".repos-table").append("<p>"+repos[i]+"</p>");
+      
+    }
+
+  })
+
 })
 
